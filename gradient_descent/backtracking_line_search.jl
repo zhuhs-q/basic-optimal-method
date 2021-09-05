ea8ec813f29b04db35482136eaf814825eddbbb3
zhuhs-q <2211059882@qq.com>
2021-09-05 16:04:02 +0800

    descent

--- gradient_descent/backtracking_line_search.jl
+++ gradient_descent/backtracking_line_search.jl
@@ -1,5 +1,3 @@
-
-
 using LinearAlgebra
 function backtracking_line_search(objective,x,alpha,beta,step,arc,gradient)
     while objective(x + step * arc) > objective(x) + alpha * step * gradient' * arc
