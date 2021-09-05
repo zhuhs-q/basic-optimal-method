58ba1959aaee294996fcae5637f2009f8dab8962
zhuhs-q <2211059882@qq.com>
2021-09-05 16:36:29 +0800

    delete gradient_descent

--- gradient_descent/backtracking_line_search.jl
+++
@@ -1,8 +0,0 @@
-using LinearAlgebra
-function backtracking_line_search(objective,x,alpha,beta,step,arc,gradient)
-    while objective(x + step * arc) > objective(x) + alpha * step * gradient' * arc
-        step = beta * step
-    end
-    return step
-end
-
